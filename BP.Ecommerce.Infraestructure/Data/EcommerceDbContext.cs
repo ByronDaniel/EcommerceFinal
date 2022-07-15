cdd21f9d6a53191e742d8cdecc5365f8668dfa12
Byron Delgado <byrondanipm@gmail.com>
2022-07-15 17:47:59 -0500

    add IEntityTypeConfiguration

--- BP.Ecommerce.Infraestructure/Data/EcommerceDbContext.cs
+++ BP.Ecommerce.Infraestructure/Data/EcommerceDbContext.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BP.Ecommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +13,14 @@ namespace BP.Ecommerce.Infraestructure.Data
     {
         public EcommerceDbContext(DbContextOptions<EcommerceDbContext> options) : base(options)
         {
+        }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
     }
 }
